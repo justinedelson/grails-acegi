ce2cc707af8250209e9cfbd0fc581a146ab333a8
burtbeckwith <burtbeckwith@832c1c66-4827-0410-8465-ccb17913cd09>
2008-06-25 13:03:00 +0000

    GRAILSPLUGINS-373
    git-svn-id: https://svn.codehaus.org/grails-plugins/grails-acegi/trunk@41862 832c1c66-4827-0410-8465-ccb17913cd09

---
+++ src/groovy/org/codehaus/groovy/grails/plugins/springsecurity/openid/GrailsOpenIdAuthenticationProvider.groovy
@@ -0,0 +1,62 @@
+package org.codehaus.groovy.grails.plugins.springsecurity.openid
+
+import org.springframework.security.Authenticationimport org.springframework.security.AuthenticationExceptionimport org.springframework.security.AuthenticationServiceException
+import org.springframework.security.BadCredentialsException
+import org.springframework.security.providers.openid.AuthenticationCancelledException
+import org.springframework.security.providers.openid.OpenIDAuthenticationStatusimport org.springframework.security.providers.openid.OpenIDAuthenticationProvider
+import org.springframework.security.providers.openid.OpenIDAuthenticationToken
+import org.springframework.security.userdetails.UserDetails
+import org.springframework.security.userdetails.UserDetailsService
+/**
+ * TODO  javadoc
+ *
+ * @author Burt
+ */
+class GrailsOpenIdAuthenticationProvider extends OpenIDAuthenticationProvider {
+
+	private UserDetailsService _userDetailsService
+
+	@Override
+	Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+		if (!supports(authentication.getClass())) {
+			return null
+		}
+
+		if (authentication instanceof OpenIDAuthenticationToken) {
+			OpenIDAuthenticationToken response = (OpenIDAuthenticationToken) authentication
+			OpenIDAuthenticationStatus status = response.status
+
+			// handle the various possibilites
+			if (status == OpenIDAuthenticationStatus.SUCCESS) {
+
+				// Lookup user details
+				UserDetails userDetails = _userDetailsService.loadUserByUsername(response.identityUrl)
+				return new GrailsOpenIdAuthenticationToken(userDetails, response.status, response.identityUrl)
+			}
+			else if (status == OpenIDAuthenticationStatus.CANCELLED) {
+				throw new AuthenticationCancelledException("Log in cancelled")
+			}
+			else if (status == OpenIDAuthenticationStatus.ERROR) {
+				throw new AuthenticationServiceException("Error message from server: " + response.message)
+			}
+			else if (status == OpenIDAuthenticationStatus.FAILURE) {
+				throw new BadCredentialsException("Log in failed - identity could not be verified")
+			}
+			else if (status == OpenIDAuthenticationStatus.SETUP_NEEDED) {
+				throw new AuthenticationServiceException(
+						"The server responded setup was needed, which shouldn't happen")
+			}
+			else {
+				throw new AuthenticationServiceException("Unrecognized return value " + status)
+			}
+		}
+		return null
+	}
+
+	@Override
+	void setUserDetailsService(UserDetailsService userDetailsService) {
+		_userDetailsService = userDetailsService
+		super.setUserDetailsService(userDetailsService)
+	}
+}
---
+++ src/groovy/org/codehaus/groovy/grails/plugins/springsecurity/openid/GrailsOpenIdAuthenticationToken.groovy
@@ -0,0 +1,26 @@
+package org.codehaus.groovy.grails.plugins.springsecurity.openid
+
+import org.springframework.security.GrantedAuthorityimport org.springframework.security.providers.openid.OpenIDAuthenticationStatusimport org.springframework.security.providers.openid.OpenIDAuthenticationToken
+import org.springframework.security.userdetails.UserDetails
+/**
+ * TODO  javadoc
+ *
+ * @author Burt
+ */
+class GrailsOpenIdAuthenticationToken extends OpenIDAuthenticationToken {
+
+	private UserDetails _userDetails
+
+	GrailsOpenIdAuthenticationToken(
+			UserDetails userDetails,
+			OpenIDAuthenticationStatus status,
+			String identityUrl) {
+		super(userDetails.authorities, status, identityUrl)
+		_userDetails = userDetails
+	}
+
+	@Override
+	Object getPrincipal() {
+		return _userDetails
+	}
+}
