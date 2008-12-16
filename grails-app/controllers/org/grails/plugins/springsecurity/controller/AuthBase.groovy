9a9a85cc40a8d2229327d0e207d3b8d8b5ea0c8b
burtbeckwith <burtbeckwith@832c1c66-4827-0410-8465-ccb17913cd09>
2008-12-16 19:18:13 +0000

    updated license copyright
    git-svn-id: https://svn.codehaus.org/grails-plugins/grails-acegi/trunk@46393 832c1c66-4827-0410-8465-ccb17913cd09

--- grails-app/controllers/org/grails/plugins/springsecurity/controller/AuthBase.groovy
+++ grails-app/controllers/org/grails/plugins/springsecurity/controller/AuthBase.groovy
@@ -1,11 +1,11 @@
-/* Copyright 2006-2007 the original author or authors.
- * 
+/* Copyright 2006-2009 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,9 +27,9 @@ import org.springframework.web.servlet.support.RequestContextUtils as RCU
 class AuthBase {
 
 	/** Authenticate Service */
-	AuthenticateService authenticateService
+	def authenticateService
 
-	/** Login user class */
+	/** Authenticated user domain instance */
 	def loginUser
 
 	/** is user logged on or not */
--- src/groovy/org/codehaus/groovy/grails/plugins/springsecurity/GrailsAuthenticationProcessingFilter.groovy
+++ src/groovy/org/codehaus/groovy/grails/plugins/springsecurity/GrailsAuthenticationProcessingFilter.groovy
@@ -1,5 +1,4 @@
-/*
- * Copyright 2008 the original author or authors.
+/* Copyright 2006-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
